4f43479b6a30eb0eac0995911fbed6cee7c59023
s-keshari2007 <shauryakeshari14@gmail.com>
2026-07-15 01:45:49 +0530

    Time: 145 ms (85.91%), Space: 196.7 MB (33.98%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,6 +1,6 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
+ * Definition for a binary tree TreeNode.
+ * struct TreeTreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
@@ -10,8 +10,69 @@
  * };
  */
 class Solution {
-public:
-    int amountOfTime(TreeNode* root, int start) {
+  public:
+    void markParents(TreeNode* root,unordered_map<TreeNode*, TreeNode*> &parent_track){
+        // we we  will be doing a level order traversal
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* current = q.front();
+            q.pop();
+            if(current->left){
+                parent_track[current->left] = current;
+                q.push(current->left);
+            }
+            if(current->right){
+                parent_track[current->right] = current;
+                q.push(current->right);
+            }
+        }
+    }
+    TreeNode* find(TreeNode* root, int n) {
+    if (root == NULL || root->val == n) {
+        return root;
+    }
+    
+    TreeNode* leftResult = find(root->left, n);
+    if (leftResult != NULL) {
+        return leftResult;
+    }
+    
+    return find(root->right, n);
+}
+    int amountOfTime(TreeNode* root, int target) {
+        // code here
+        unordered_map<TreeNode*, TreeNode*> parent_track;
+        markParents(root, parent_track);
         
+        TreeNode* newtarget = find(root,target);
+        
+        
+        unordered_map<TreeNode*, bool> visited;
+        queue<TreeNode*>q;
+        q.push(newtarget);
+        visited[newtarget] = true;
+        int samay = 0;
+        while(!q.empty()){
+            int size = q.size();
+            samay++;
+            for(int i = 0; i<size; i++){
+                TreeNode* current = q.front();
+                q.pop();
+                if(current->left && !visited[current->left]){
+                    q.push(current->left);
+                    visited[current->left] = true;
+                }
+                if(current->right && !visited[current->right]){
+                    q.push(current->right);
+                    visited[current->right] = true;
+                }
+                if(parent_track[current] && !visited[parent_track[current]]){
+                    q.push(parent_track[current]);
+                    visited[parent_track[current]] = true;
+                }
+            }
+        }
+        return samay-1; 
     }
 };
