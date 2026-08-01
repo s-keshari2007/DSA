eb78cdbdd5eeb45b8094b7d78e144d41b74ac709
s-keshari2007 <shauryakeshari14@gmail.com>
2026-08-01 22:54:03 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.35%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int result = 0;
+        // Taking XOR with a number with itself gives 0
+        // taking XOR with 0 gives the same number itself
         for (int num : nums) {
             result ^= num;
         }
