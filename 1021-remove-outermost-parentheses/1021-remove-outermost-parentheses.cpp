d655451c74977bec58dcb783716c5896e833446b
s-keshari2007 <shauryakeshari14@gmail.com>
2026-08-11 14:58:06 +0530

    Time: 0 ms (100%), Space: 8.8 MB (81.73%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,22 +1,41 @@
+// class Solution {
+// public:
+//     string removeOuterParentheses(string s) {
+//         stack<char> st;
+//         string ans = "";
+//         for(auto ch : s){
+//             if(ch == '(' && st.size() == 0){
+//                 st.push(ch);
+//             }
+//             else if(st.size() != 0 && ch == '('){
+//                 st.push('(');
+//                 ans = ans + '(';
+//             }
+//             else if(ch == ')' && st.size() != 1){
+//                 st.pop();
+//                 ans = ans + ')'; 
+//             }
+//             else {
+//                 st.pop();
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        stack<char> st;
-        string ans = "";
-        for(auto ch : s){
-            if(ch == '(' && st.size() == 0){
-                st.push(ch);
-            }
-            else if(st.size() != 0 && ch == '('){
-                st.push('(');
-                ans = ans + '(';
-            }
-            else if(ch == ')' && st.size() != 1){
-                st.pop();
-                ans = ans + ')'; 
-            }
-            else {
-                st.pop();
+        string ans;
+        ans.reserve(s.size()); // avoid reallocations
+        int depth = 0;
+
+        for (char ch : s) {
+            if (ch == '(') {
+                if (depth > 0) ans.push_back(ch);
+                depth++;
+            } else { // ch == ')'
+                depth--;
+                if (depth > 0) ans.push_back(ch);
             }
         }
         return ans;
