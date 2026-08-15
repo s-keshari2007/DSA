6ac19efb0ad2579bff5dd89ffeecc16ae09ed45b
s-keshari2007 <shauryakeshari14@gmail.com>
2026-08-15 23:39:26 +0530

    Time: 2 ms (50%), Space: 9.7 MB (76.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,38 @@
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
 
-        if(s.length() != t.length()) return false;
+//         if(s.length() != t.length()) return false;
 
-        int n = s.length();
+//         int n = s.length();
         
-        int sl[26] = {};
-        int tl[26] = {};
+//         int sl[26] = {};
+//         int tl[26] = {};
 
-        for(int i = 0; i <n; i++){
-            sl[s[i]  - 'a']++;
-            tl[t[i]  - 'a']++;
-        }
-        for(int i = 0; i < 26; i++){
-            if(sl[i] != tl[i]) return false;
-        }
-        return true;
+//         for(int i = 0; i <n; i++){
+//             sl[s[i]  - 'a']++;
+//             tl[t[i]  - 'a']++;
+//         }
+//         for(int i = 0; i < 26; i++){
+//             if(sl[i] != tl[i]) return false;
+//         }
+//         return true;
+//     }
+// };
+class Solution {
+    public:
+bool isAnagram(string s, string t) {
+    if (s.length() != t.length()) return false;
+
+    unordered_map<char32_t, int> counts;
+    // (assuming s and t are already decoded into sequences of code points,
+    // e.g. via char32_t or a Unicode-aware string type)
+
+    for (char32_t c : s) counts[c]++;
+    for (char32_t c : t) {
+        if (counts.find(c) == counts.end()) return false;
+        if (--counts[c] < 0) return false;
     }
+    return true;
+}
 };
