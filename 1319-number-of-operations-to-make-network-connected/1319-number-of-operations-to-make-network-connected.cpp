1fa0ae9026ff4b2433d51583a7531b6c5b61e456
s-keshari2007 <shauryakeshari14@gmail.com>
2026-08-19 22:35:11 +0530

    Time: 28 ms (25.17%), Space: 47.1 MB (32.6%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,30 +1,81 @@
-class Solution {
-private:
-    void DFS(int node, vector<vector<int>>& adj, vector<int>& vis){
-        vis[node] = 1;
-        for(auto it : adj[node]){
-            if(!vis[it]) DFS(it, adj, vis);
-        }
-    }
-public:
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        if((int)connections.size() < n - 1) return -1;
+class DisjointSet{
+    
+    public:
+    vector<int> parent, rank, size;
 
-        vector<vector<int>> adj(n);
-        for(auto& it : connections){
-            int u = it[0], v = it[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-
-        vector<int> vis(n, 0);
-        int components = 0;
-        for(int i = 0; i < n; i++){
-            if(!vis[i]){
-                DFS(i, adj, vis);
-                components++;
+        DisjointSet(int n){
+            // this done using 1  based indexing
+            rank.resize(n+1, 0);
+            parent.resize(n+1, 0);
+            size.resize(n+1 ,0); 
+            for(int i = 0; i <= n; i++){
+                parent[i] = i; //firstly  everybody is the parent of itself
+                size[i] = 1;
             }
         }
-        return components - 1;
+
+        int FindUpar(int node){
+            if(node == parent[node]) return node;
+
+            return parent[node] = FindUpar(parent[node]);
+        }
+
+        void unionByRank(int u, int v){
+            int ulp_u = FindUpar(u);
+            int ulp_v = FindUpar(v);
+            if(ulp_u == ulp_v) return;
+            if(rank[ulp_u] < rank[ulp_v]){
+                parent[ulp_u] = ulp_v;
+            }
+            else{
+                parent[ulp_v] = ulp_u;
+                rank[ulp_u]++;
+            }
+        }
+        void unionBySize(int u, int v){
+            int ulp_u = FindUpar(u);
+            int ulp_v = FindUpar(v);
+            if(ulp_u == ulp_v) return;
+            if(size[ulp_u] < size[ulp_v]){
+                parent[ulp_u] = ulp_v;
+                size[ulp_v] += size[ulp_u];
+            }
+            else{
+                parent[ulp_v] = ulp_u;
+                size[ulp_u] += size[ulp_v];
+             }
+        }
+
+
+};
+
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjointSet ds(n);
+        int cntextra = 0;
+        for(auto it:connections){
+            int u = it[0];
+            int v = it[1];
+
+            if(ds.FindUpar(u) == ds.FindUpar(v)){
+                cntextra++;
+            }
+            else{
+                ds.unionBySize(u,v);
+            }
+        }
+        int cntComponent = 0;
+        for(int i = 0; i < n; i++){  
+            if(ds.parent[i] == i){ //when a element points towartds itself it mean it a component boss
+                cntComponent++;
+            }
+        } 
+        int ans = cntComponent - 1; // cntComponent - 1 is the number of wires to connect cntComponents
+        if(cntextra  >= ans){
+            return ans;
+        }
+        return -1;
     }
 };
