be9f3a94b7c49c8eef5f17c9e3162318071c04db
Shaurya <shauryakeshari14@gmail.com>
2026-09-03 23:39:22 +0530

    Time: 78 ms (78.04%), Space: 239.7 MB (38.28%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,22 +1,26 @@
 class Solution {
+    unordered_map<int,int> mpp;      // stone value -> index
+    int n;
+    vector<vector<int>> memo;        // memo[pos][jump]: -1 unknown, 0 false, 1 true
+    bool solve(int pos, int jump, vector<int>& stones) {
+        if (pos == n - 1) return true;
+        if (pos < 0 || pos >= n) return false;              // defensive
+        if (memo[pos][jump] != -1) return memo[pos][jump];  // cached!
+        for (int k = -1; k <= 1; ++k) {
+            int nextJump = jump + k;
+            if (nextJump <= 0) continue;
+            if (mpp.find(stones[pos] + nextJump) != mpp.end())
+                if (solve(mpp[stones[pos] + nextJump], nextJump, stones))
+                    return memo[pos][jump] = true;
+        }
+        return memo[pos][jump] = false;
+    }
 public:
     bool canCross(vector<int>& stones) {
-        std::unordered_map<int, std::unordered_set<int>> dp;
-        for (int stone : stones) {
-            dp[stone] = std::unordered_set<int>();
-        }
-        dp[0].insert(0);
-
-        for (int stone : stones) {
-            for (int jump : dp[stone]) {
-                for (int jumpDistance : {jump - 1, jump, jump + 1}) {
-                    if (jumpDistance > 0 && dp.find(stone + jumpDistance) != dp.end()) {
-                        dp[stone + jumpDistance].insert(jumpDistance);
-                    }
-                }
-            }
-        }
-
-        return !dp[stones.back()].empty();        
+        if (stones[1] != 1) return false;
+        n = stones.size();
+        memo.assign(n, vector<int>(n + 1, -1));
+        for (int i = 0; i < stones.size(); ++i) mpp[stones[i]] = i;
+        return solve(0, 0, stones);
     }
 };
